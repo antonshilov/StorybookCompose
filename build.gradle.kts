84b939269814a83e0087b3a24e25ef2a72ae4c98
Anton Shilov <vaccum121@gmail.com>
2021-06-01 14:41:45 +0300

    Upgrade to 0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("com.android.tools.build:gradle:4.0.1")
     }
 }
